8c5bdd0bc644a16b2b1c2c275e497b8d9617a16a
shashankanakal24 <shashankanakal2004@gmail.com>
2024-12-22 01:32:38 +0530

    draft version.one

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+from predict import predict
+import os
+
+app = Flask('image_description')
+
+@app.route('/')
+def show_predict_stock_form():
+    return render_template('home.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = request.form
+    if request.method == 'POST':
+        myfile = request.files['myfile']
+        predicted_desc = predict(myfile).replace('start ','').replace('end','')
+        del myfile
+        return predicted_desc
+
+
+@app.route('/refresh')
+def refresh():
+    return render_template('body.html')
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", "9999", debug=True, threaded=False)
